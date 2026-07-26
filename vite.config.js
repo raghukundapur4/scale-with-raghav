d00253bc3736c5839f42c0788b4105e8dbf82e79
Raghavendra M <raghukundapur4@gmail.com>
2026-07-26 23:49:06 +0530

    configure custom domain asset paths

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // When deploying to GitHub Pages under a repo subpath, set base to the repo name.
-  // Adjust this to your repo name (including leading and trailing slash).
-  base: '/scale-with-raghav/',
+  base: '/',
 })
